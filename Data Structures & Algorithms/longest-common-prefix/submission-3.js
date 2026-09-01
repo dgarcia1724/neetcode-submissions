d48c23d09b78ab6abf84bb648d00a670e6511c23
Daniel Garcia <47014730+dgarcia1724@users.noreply.github.com>
2026-09-01 01:35:22 -0700

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let prefix = strs[0]
+        for (let i = 1; i < strs.length; i++) {
+            let currWord = strs[i]
+            let j = 0
+            while (j < currWord.length && j < prefix.length) {
+                if (currWord[j] != prefix[j]) {
+                    break
+                }
+                j++
+            }
+            prefix = prefix.slice(0,j)
+        }
+        return prefix
+    }
+}
