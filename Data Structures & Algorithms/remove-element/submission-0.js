f50085c3ff5d21d4a58b5f0ba17accd90e4c98ab
Daniel Garcia <47014730+dgarcia1724@users.noreply.github.com>
2026-09-01 22:09:05 -0700

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let k = 0;
+        for (let i =0; i < nums.length; i++) {
+            if (val != nums[i]) {
+                nums[k] = nums[i]
+                k++
+            }
+
+        }
+        return k
+    }
+}
