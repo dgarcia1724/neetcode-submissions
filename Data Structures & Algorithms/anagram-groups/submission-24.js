dacebeb1a580b944c67e95aec056b718fcd7dde1
Daniel Garcia <47014730+dgarcia1724@users.noreply.github.com>
2026-09-01 21:37:29 -0700

    Add: anagram-groups - submission-24

---
+++ Data Structures & Algorithms/anagram-groups/submission-24.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = {}
+
+        for (const s of strs) {
+            const count = new Array(26).fill(0)
+            for (const char of s) {
+                count[char.charCodeAt(0)-"a".charCodeAt(0)]++
+            }
+            const key = count.join(",");
+            if (!res[key]) res[key] = [];
+            res[key].push(s)
+        }
+
+        return Object.values(res)
+    }
+}
