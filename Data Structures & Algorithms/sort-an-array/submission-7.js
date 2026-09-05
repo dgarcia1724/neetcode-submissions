2176353ad9e3d0b5dd92db58908f98df080f2b52
Daniel Garcia <47014730+dgarcia1724@users.noreply.github.com>
2026-09-05 05:31:20 -0700

    Add: sort-an-array - submission-7

---
+++ Data Structures & Algorithms/sort-an-array/submission-7.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums) {
+        const quickSort = (arr) => {
+            if (arr.length <= 1) return arr 
+
+            const pivot = arr[arr.length-1]
+            const rest = arr.slice(0, arr.length-1)
+
+            const left = rest.filter(x=>x < pivot)
+            const right = rest.filter(x=>x >= pivot)
+            return [...quickSort(left), pivot, ...quickSort(right)]
+
+        }
+
+        return quickSort(nums)
+    }
+}
